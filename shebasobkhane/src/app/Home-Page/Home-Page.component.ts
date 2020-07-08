5476d1379365c1b537c93b774bb1bc835d5d11db
Nobel-ruettt <nobelnurulislam@gmail.com>
2020-07-08 22:59:21 +0600

    First commit

---
+++ shebasobkhane/src/app/Home-Page/Home-Page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Home-Page',
+  templateUrl: './Home-Page.component.html',
+  styleUrls: ['./Home-Page.component.css']
+})
+export class HomePageComponent implements OnInit {
+  focus: any;
+  focus1: any;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
